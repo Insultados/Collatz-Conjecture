05bea764259a8cd3b355f3ecd83374df4d50b754
Insultados <90966500+Insultados@users.noreply.github.com>
2022-06-13 01:01:08 +0300

    update 13.06

--- scripts/index.js
+++ scripts/index.js
@@ -516,7 +516,7 @@ function createRule() {
     if (array_includes(userIf, [0, 0]) == false && array_includes(userAction, [0, 0]) == false && checkElseifZero && userIf.includes(-1) == false
         && userElseif.includes(-1) == false && userAction.includes(-1) == false && Number.isInteger(parseInt($("#action_condition_input_else1").val()))
         && Number.isInteger(parseInt($("#action_condition_input_else0").val())) && !($("#action_condition_input_else1").val()[0] == '0' && $("#action_condition_input_else1").val().length > 1)
-        && !($("#action_condition_input_else0").val()[0] == '0' && $("#action_condition_input_else0").val().length > 1) && parseInt($("#action_condition_input_else0").val())) {
+        && !($("#action_condition_input_else0").val()[0] == '0' && $("#action_condition_input_else0").val().length > 1) &&  Number.isInteger(parseInt($("#action_condition_input_else0").val()))) {
 
         if (check_createRule == false) {
             $("#rule_text").remove();
